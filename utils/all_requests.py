9c13661a9f9fcf70b31d6623a5f53ad8e6e85676
Stanislav <69276462+stasgoodman@users.noreply.github.com>
2022-02-17 23:05:10 +0200

    Update all_requests.py

--- utils/all_requests.py
+++ utils/all_requests.py
@@ -6,6 +6,19 @@ class GotoBin:
 
     def __init__(self):
         self.dread = DataReader()
+        self.headers_bearer = {'Content-Type': 'application/json',
+                               'Authorization': 'Bearer '+self.dread.bearer_token}
+        self.headers_post = {'Content-Type': 'application/json',
+                             'Authorization': 'Bearer '+self.dread.bearer_token,
+                             'OrderId': str(self.dread.q_uuid)}
 
-    def get_any(self):
-        return requests.get(url=f"{self.dread.protocol}://{self.dread.host}/?UUID={self.dread.uuid}")
+    def get_any_uuid(self):
+        return requests.get(url=f"{self.dread.protocol}://{self.dread.host}/?UUID={self.dread.q_uuid}")
+
+    def get_any_bearer(self):
+        return requests.get(url=f"{self.dread.protocol}://{self.dread.host}/bearer",
+                            headers=self.headers_bearer)
+
+    def post_any_post(self):
+        return requests.post(url=f"{self.dread.protocol}://{self.dread.host}/post",
+                            headers=self.headers_post)
